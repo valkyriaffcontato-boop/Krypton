518bd3938ee9ffe3362318e554605ee09e73ec61
valkyriaffcontato-boop <valkyriaff.contato@gmail.com>
2026-06-22 11:15:01 -0300

    Create eventHandler.js

---
+++ handlers/eventHandler.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (client) => {
+  const eventsPath = path.join(__dirname, '../events');
+  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+  for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args, client));
+    }
+  }
+  console.log(`[BOT] Eventos de sistema carregados.`);
+};
