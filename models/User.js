5fc561a5cbea6356c719d2f45dfaf4d212fd6451
bielcalvoconstant-lab <bielcalvo.constant@gmail.com>
2026-06-24 16:13:41 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  discordId: { type: String, required: true, unique: true },
-  email: { type: String, default: null },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  discordId: { type: String, default: null },
   isVerified: { type: Boolean, default: false },
   otp: { type: String, default: null },
   otpExpires: { type: Date, default: null },
-  role: { type: String, default: 'user' } // 'user' ou 'superadmin' para mafiosodashopping@gmail.com
+  resetToken: { type: String, default: null },
+  resetTokenExpires: { type: Date, default: null },
+  role: { type: String, default: 'user' }, // 'superadmin' para mafiosodashopping@gmail.com
+  isVip: { type: Boolean, default: false }, // Status de assinatura VIP real
+  stripeCustomerId: { type: String, default: null } // ID do Cliente no Stripe
 });
 
 module.exports = mongoose.model('User', UserSchema);
